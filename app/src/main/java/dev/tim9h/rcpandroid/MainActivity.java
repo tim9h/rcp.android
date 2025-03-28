9a4ceeddfc4da5679966df28e66b89b7b742826b
Tim <tim@heidelba.ch>
2025-03-28 14:01:53 +0100

    Add menu with item settings

--- app/src/main/java/dev/tim9h/rcpandroid/MainActivity.java
+++ app/src/main/java/dev/tim9h/rcpandroid/MainActivity.java
@@ -1,10 +1,12 @@
 package dev.tim9h.rcpandroid;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -12,9 +14,10 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.color.DynamicColors;
 
 import dev.tim9h.rcpandroid.databinding.ActivityMainBinding;
-import dev.tim9h.rcpandroid.ui.utils.BindingUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         var appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_media, R.id.navigation_system, R.id.navigation_lighting)
                 .build();
-        var navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
@@ -38,6 +41,21 @@ public class MainActivity extends AppCompatActivity {
     private static void disableMenu(ActivityMainBinding binding, int id) {
         var menuItem = binding.navView.getMenu().findItem(id);
         menuItem.setEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        var id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
