d5f6f0ef2561883ab102f80937cec27cad537278
Tim <tim@heidelba.ch>
2025-04-26 16:03:54 +0200

    Add last.fm settings

--- app/src/main/java/dev/tim9h/rcpandroid/ui/settings/SettingsFragment.java
+++ app/src/main/java/dev/tim9h/rcpandroid/ui/settings/SettingsFragment.java
@@ -40,39 +40,33 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.settings, rootKey);
 
-        var user = findPreference("rest_api_username");
-        if (user != null) {
-            user.setSummary(getStoredValue("rest_api_username", "Not set"));
-            user.setOnPreferenceChangeListener((preference, newValue) -> {
-                preference.setSummary(newValue.toString());
-                return true;
-            });
-        }
+        decorateSetting("rest_api_username", false);
+        decorateSetting("rest_api_password", true);
+        decorateSetting("rest_base_url", false);
+        decorateSetting("lastfm_username", false);
+        decorateSetting("lastfm_apikey", true);
+    }
 
-        EditTextPreference password = findPreference("rest_api_password");
-        if (password != null) {
-            password.setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD));
-            password.setSummary(getStoredValue("rest_api_password", "").isEmpty() ? "Not set" : "••••••••");
-            password.setOnPreferenceClickListener(preference -> {
+    private void decorateSetting(String preferenceKey, boolean isPassword) {
+        EditTextPreference preference = findPreference(preferenceKey);
+        if (preference != null) {
+            if (isPassword) {
+                preference.setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD));
                 preference.setSummary("••••••••");
-                return true;
-            });
-        }
-
-        EditTextPreference port = findPreference("rest_base_url");
-        if (port != null) {
-            port.setSummary(getStoredValue("rest_base_url", "Not set"));
-            port.setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_TEXT_VARIATION_URI));
-            port.setOnPreferenceChangeListener((preference, newValue) -> {
-                preference.setSummary(newValue.toString());
-                return true;
-            });
+            } else {
+                preference.setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_TEXT_VARIATION_URI));
+                preference.setSummary(getStoredValue(preferenceKey));
+                preference.setOnPreferenceChangeListener((pref, value) -> {
+                    pref.setSummary(value.toString());
+                    return true;
+                });
+            }
         }
     }
 
-    private String getStoredValue(String key, String defaultValue) {
+    private String getStoredValue(String key) {
         return PreferenceManager.getDefaultSharedPreferences(requireContext())
-                .getString(key, defaultValue);
+                .getString(key, "Not set");
     }
 
 }
